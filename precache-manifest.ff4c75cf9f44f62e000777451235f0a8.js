88a78c394ca638d36dd7476a676f51a4e5bc848f
narine <narinemkn@gmail.com>
2020-07-10 20:59:05 +0200

    Updates

---
+++ precache-manifest.ff4c75cf9f44f62e000777451235f0a8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39bea44546a87b9b10b5eeb66aa6d091",
+    "url": "/19Nar/101CookbooksAPIReact/index.html"
+  },
+  {
+    "revision": "4f5b81c1c648964ee5fc",
+    "url": "/19Nar/101CookbooksAPIReact/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "003b10f78384184d72e8",
+    "url": "/19Nar/101CookbooksAPIReact/static/css/main.735880b5.chunk.css"
+  },
+  {
+    "revision": "4f5b81c1c648964ee5fc",
+    "url": "/19Nar/101CookbooksAPIReact/static/js/2.dbb371be.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/19Nar/101CookbooksAPIReact/static/js/2.dbb371be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "003b10f78384184d72e8",
+    "url": "/19Nar/101CookbooksAPIReact/static/js/main.2634f336.chunk.js"
+  },
+  {
+    "revision": "df5e27b5ce98f872b6e1",
+    "url": "/19Nar/101CookbooksAPIReact/static/js/runtime-main.cfa04fe7.js"
+  },
+  {
+    "revision": "06ac5430542eec308943d581e11ffd60",
+    "url": "/19Nar/101CookbooksAPIReact/static/media/WELCOME.06ac5430.jpg"
+  },
+  {
+    "revision": "8f2e05e0187a35321a3548f9b96e121a",
+    "url": "/19Nar/101CookbooksAPIReact/static/media/facebook.8f2e05e0.jpg"
+  },
+  {
+    "revision": "dd1c3c6b6d4575352d92d0f703d40bb9",
+    "url": "/19Nar/101CookbooksAPIReact/static/media/insta.dd1c3c6b.jpg"
+  },
+  {
+    "revision": "3fea4d66c5352dfc01c4491b8dae6347",
+    "url": "/19Nar/101CookbooksAPIReact/static/media/youtube.3fea4d66.jpg"
+  }
+]);
