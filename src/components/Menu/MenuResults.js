4eb405e3f0f05fd546ab28493251b37f27a27a43
narine <narinemkn@gmail.com>
2020-08-12 23:38:26 +0200

    +

--- src/components/Menu/MenuResults.js
+++ src/components/Menu/MenuResults.js
@@ -5,7 +5,6 @@ import {
   CardImg,
   CardText,
   CardBody,
-  CardTitle,
   CardLink,
 } from "reactstrap";
 
@@ -13,25 +12,20 @@ import LikeButton from "./LikeButton";
 
 import "./Menu.css";
 
-const MenuResults = ( {meals}, item )  => (        
-  <React.Fragment key={item.id}>                                                                                                                                                                                                                      
-    <Col className="col" xs="12" sm="12" md="6" lg="6" xl="6">
-      <hr></hr>
+const MenuResults = ({ meals }, item) => (
+  <React.Fragment key={item.id}>
+    <Col className="col" xs="12" sm="12" md="4" lg="4" xl="4">
       <Card className="mycard">
         <CardImg src={meals.image_url} alt="Card image cap" />
         <CardBody>
-          <div  className="card-img-overlay">
-            <CardTitle>Title: {meals.title}</CardTitle>
-            <CardText className="text">Recipe ID: {meals.recipe_id}</CardText>
-            <p>See more</p>
-            <CardLink href={meals.publisher_url}> Publisher</CardLink>
-            <CardLink href={meals.source_url}> Source</CardLink>
-            </div>
+          <CardText className="text">Recipe ID: {meals.recipe_id}</CardText>
+          <CardLink href={meals.publisher_url}> Publisher</CardLink>
+          <CardLink href={meals.source_url}> Source</CardLink>
+          <LikeButton itemId={item.id} />
         </CardBody>
       </Card>
-      <LikeButton itemId={item.id} />
     </Col>
-    </React.Fragment>
-  );
+  </React.Fragment>
+);
 
 export default MenuResults;
