6cd9b2c0a7651ed30f49d7fb36e2e5310881807d
narine <narinemkn@gmail.com>
2020-08-13 17:28:39 +0200

    +

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 import CarouselResults from "./CarouselResults";
+import Text from "./Text";
+
 import "./Home.css";
 
 const Home = () => {
   return (
     <>
       <CarouselResults />
+      <Text />
     </>
   );
 };
