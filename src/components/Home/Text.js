31930befcfe641d5ab26cada19deb5337e03efc3
narine <narinemkn@gmail.com>
2020-08-13 18:58:02 +0200

    +

--- src/components/Home/Text.js
+++ src/components/Home/Text.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from "react";
 
-function Text() {
-    return (
-        <div>
-           <h1>100+ Vegetarian Recipes</h1>
-          <p>
-            If you’re on the quest for vegetarian
-            recipes, you’re in the right place. All of the recipes on 101
-            Cookbooks are vegetarian, but(!) this section highlights some
-            favorites, along with many ideas for mains and side dishes to build
-            a meal around.
-          </p> 
-        </div>
-    )
-}
+import "./Home.css";
 
-export default Text
+const Text = () => {
+  return (
+    <div>
+      <p className="vegetarian">
+        <h1>100+ Vegetarian Recipes</h1>
+        If you’re on the quest for vegetarian recipes, you’re in the right
+        place. All of the recipes on 101 Cookbooks are vegetarian, but(!) this
+        section highlights some favorites, along with many ideas for mains and
+        side dishes to build a meal around.
+      </p>
+    </div>
+  );
+};
+
+export default Text;
